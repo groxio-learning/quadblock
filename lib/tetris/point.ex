a5bf58d590de61389ac9da4566ad7fa97a1bfff0
Bruce Tate <bruce@rapidred.com>
2020-05-16 06:59:06 -0400

    initial point, tetromino

---
+++ lib/tetris/point.ex
@@ -0,0 +1,17 @@
+defmodule Tetris.Point do
+  def origin() do
+    {0, 0}
+  end
+  
+  def left({x, y}) do
+    {x-1, y}
+  end
+  
+  def right({x, y}) do
+    {x+1, y}
+  end
+
+  def down({x, y}) do
+    {x, y+1}
+  end
+end
