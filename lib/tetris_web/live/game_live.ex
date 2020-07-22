dc6457cbf0edceb7af07a4dbb3cb6c0b3c0b5845
Brett Wise <brettdwise@gmail.com>
2020-07-22 10:39:04 -0500

    Formatting

--- lib/tetris_web/live/game_live.ex
+++ lib/tetris_web/live/game_live.ex
@@ -1,31 +1,31 @@
 defmodule TetrisWeb.GameLive do
   use TetrisWeb, :live_view
   alias Tetris.Tetromino
-  
 
   def mount(_params, _session, socket) do
     :timer.send_interval(500, :tick)
+
     {
-      :ok, 
-      socket 
+      :ok,
+      socket
       |> new_tetromino
       |> show
     }
   end
-  
+
   def render(assigns) do
     ~L"""
     <% [{x, y}] = @points %>
     <section class="phx-hero">
       <h1>Welcome to Tetris</h1>
       <%= render_board(assigns) %>
-      <pre> 
+      <pre>
         {<%= x %>, <%= y %>}
       </pre>
     </section>
     """
   end
-  
+
   defp render_board(assigns) do
     ~L"""
     <svg width="200" height="400">
@@ -34,37 +34,36 @@ defmodule TetrisWeb.GameLive do
     </svg>
     """
   end
-  
-  defp render_points(%{points: [{x, y}]}=assigns) do
+
+  defp render_points(%{points: [{x, y}]} = assigns) do
     ~L"""
-    <rect 
-      width="20" height="20" 
-      x="<%= (x - 1) * 20 %>" y="<%= (y - 1) * 20 %>" 
+    <rect
+      width="20" height="20"
+      x="<%= (x - 1) * 20 %>" y="<%= (y - 1) * 20 %>"
       style="fill:rgb(255,0,0);" />
     """
   end
-  
+
   defp new_tetromino(socket) do
     assign(socket, tetro: Tetromino.new_random())
   end
-  
+
   defp show(socket) do
-    assign(socket, 
+    assign(socket,
       points: Tetromino.points(socket.assigns.tetro)
     )
   end
-  
 
-  def down(%{assigns: %{tetro: %{location: {_, 20}}}}=socket) do
+  def down(%{assigns: %{tetro: %{location: {_, 20}}}} = socket) do
     socket
     |> new_tetromino
     |> show
   end
 
-  def down(%{assigns: %{tetro: tetro}}=socket) do
+  def down(%{assigns: %{tetro: tetro}} = socket) do
     assign(socket, tetro: Tetromino.down(tetro))
   end
-  
+
   def handle_info(:tick, socket) do
     {:noreply, socket |> down |> show}
   end
